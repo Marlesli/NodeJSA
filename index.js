e253ea8a878824c2edeb2213d08433559c77853e
Marlesli <122844039+Marlesli@users.noreply.github.com>
2025-04-03 16:34:00 -0400

    ejs CRUD

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParseer = require("body-parser");
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded: false}));
+
+var tasks =["create to do list", "add items"]
 
 app.get('/', function(req, res) {
    // res.send('New Node App');
-   res.render('index', {userName: req.query.userName || 'CPSC'});
+   res.render('index', {tasks: tasks || []});
 })
 
-app.get('/name/:userName', function(req, res) {
-    res.render('index.ejs', {userName: req.params.userName});
+app.post('/add', function(req, res) {
+    console.log(req)
+    tasks.push(req.body.newTask)
+    res.redirect('/');
  })
  
 app.listen(3000, function() {
     console.log('Our app is running on port 3000');
 })
 
+//CRUD -> To do = fetch post put delete
